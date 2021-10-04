241be1b0fdd37ce517e0353385cbbeb419e3633c
jhoman81 <josh.homan@gmail.com>
2021-10-04 15:18:24 -0500

    Updated DbContext

--- SRS.EntityFramework/SRSDbContext.cs
+++ SRS.EntityFramework/SRSDbContext.cs
@@ -13,6 +13,17 @@ namespace SRS.EntityFramework
     {
         public DbSet<User> Users {  get; set; }
         public DbSet<Project> Projects {  get; set; }
+        public DbSet<Community> Communities { get; set; }
+        public DbSet<Event> Events {  get; set; }   
+        public DbSet<Fact> Facts {  get; set; } 
+        public DbSet<Family> Families {  get; set; }
+        public DbSet<Household> Households { get; set; }
+        public DbSet<Media> Media {  get; set; }
+        public DbSet<Note> Notes { get; set }
+        public DbSet<Person> People { get; set; }
+        public DbSet<Place> Places { get; set; }
+        public DbSet<Reference> References {  get; set; }   
+        public DbSet<Relation> Relations {  get; set; }
         public string DbPath { get; set; }
         public SRSDbContext(DbContextOptions options) : base(options) { }
 
